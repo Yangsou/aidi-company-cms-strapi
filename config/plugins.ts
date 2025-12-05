004910a920100044b5270b77e5ae35932866968c
Nguyen Trung Nam <nam.nguyen@aidi.world>
2025-12-05 17:29:54 +0700

    feat: install plugin export/import csv

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,29 @@
-export default () => ({});
+export default () => ({
+  "strapi-csv-import-export": {
+    enable: true,
+    config: {
+      authorizedExports: [
+        "api::machine.machine",
+        "api::article.article",
+        "api::job-location.job-location",
+        "api::job-tag.job-tag",
+        "api::product.product",
+        "api::team-member.team-member",
+        "api::activity.activity",
+        "api::category.category",
+        "api::job.job"
+      ],
+      authorizedImports: [
+        "api::machine.machine",
+        "api::article.article",
+        "api::job-location.job-location",
+        "api::job-tag.job-tag",
+        "api::product.product",
+        "api::team-member.team-member",
+        "api::activity.activity",
+        "api::category.category",
+        "api::job.job"
+      ],
+    },
+  },
+});
